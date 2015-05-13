4f15662c820f162e1a55e721f7d10034c82baf54
Belousov Pavel <belousovpavel@gmail.com>
2015-05-13 10:43:59 +0300

    Revert "Remove parsing query string when empty values"
    This reverts commit 3f5c47a10309fa71057dfa95fdf009f1acd48a21.

--- jquery.redirect.js
+++ jquery.redirect.js
@@ -13,11 +13,18 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 /**
  * jQuery Redirect
  * @param {string} target - Url of the redirection
- * @param {Object} values - (optional) An object with the data to send.
+ * @param {Object} values - (optional) An object with the data to send. If not present will look for values as QueryString in the target url.
  * @param {string} method - (optional) The HTTP verb can be GET or POST (defaults to POST)
  */
 	$.redirect = function( target, values, method ) {  
 		method = (method && method.toUpperCase() == 'GET') ? 'GET' : 'POST';
+			
+		if (!values)
+		{
+			var obj = $.parse_url(target);
+			target = obj.url;
+			values = obj.params;
+		}
 					
 		var form = $('<form>').attr({
 			method: method,
@@ -30,6 +37,25 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 	};
 
 //Private Functions	
+	$.parse_url = function(url)
+	{
+		if (url.indexOf('?') == -1)
+			return { url: url, params: {} }
+			
+		var parts = url.split('?'),
+			url = parts[0],
+			query_string = parts[1],
+			elems = query_string.split('&'),
+			obj = {};
+		
+		for(var i in elems)
+		{
+			var pair = elems[i].split('=');
+			obj[pair[0]] = pair[1];
+		}
+
+		return {url: url, params: obj};		
+	};  	
 	var getInput = function(name, value, parent) {
 		var parentString;
 		if( parent.length > 0 ) {
